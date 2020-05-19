ce9faab17e953f6b45b65948d86611134e69bb6a
uronohan <uronohan@gmail.com>
2020-05-19 11:14:17 +0700

    minor chnage

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const db = require('./models');
 const express = require('express');
 const app = express();
 
-const contact_usRoutes = require('./routes/contact_us')
-
-
+const contact_usRoutes = require('./routes/contact_us');
+const loginRoutes = require('./routes/login');
+const postRoutes = require('./routes/post');
+const userRoutes = require('./routes/user');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/contact_us', contact_usRoutes)
 
+
+
+app.use('/contact_us', contact_usRoutes)
+app.use('/login', loginRoutes)
+app.use('./post', postRoutes)
+app.use('./user', userRoutes)
 
 
 
@@ -19,3 +25,4 @@ db.sequelize.sync().then(() => {
         console.log('Server is running on port 8000');
     });
 });
+
