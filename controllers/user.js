66346b18e5d696d75bec40e479b40a40619dade1
uronohan <uronohan@gmail.com>
2020-05-21 15:58:45 +0700

    minor route and controller change

--- controllers/user.js
+++ controllers/user.js
@@ -1,1 +1,15 @@
 const db = require('../models');
+
+const getAllUsers = (req,res) => {};
+const getUsersById = (req,res) => {};
+const createNewUsers = (req,res) => {};
+const editUsersById = (req,res) => {};
+const deleteUsersById = (req,res) => {};
+
+module.exports = {
+    getAllUsers,
+    getUsersById,
+    createNewUsers,
+    editUsersById,
+    deleteUsersById,
+};
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/user')
 
-
-
+router.get('/', userController.getAllUsers);
+router.get('/', userController.getUsersById);
+router.post('/', userController.createNewUsers);
+router.put('/', userController.editUsersById);
+router.delete('/', userController.deleteUsersById);
 
 
 module.exports = router;
