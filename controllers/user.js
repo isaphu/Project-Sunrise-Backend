33102b77ff3ff1099329ef4617528a968b46a060
uronohan <uronohan@gmail.com>
2020-05-25 13:35:29 +0700

    minor change in controllers

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,10 @@
 const db = require('../models');
 
-const getAllUsers = (req,res) => {};
+const getAllUsers = (req,res) => {
+    const users = await db.User.findAll();
+
+    res.send(users);
+};
 const getUsersById = (req,res) => {};
 const createNewUsers = (req,res) => {};
 const editUsersById = (req,res) => {};
