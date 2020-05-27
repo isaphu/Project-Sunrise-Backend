304487f18714702ff38ee7bff54cb50aa84eeef3
uronohan <uronohan@gmail.com>
2020-05-27 15:07:14 +0700

    changes in controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,16 @@
 const db = require('../models');
 
-const getAllUsers = (req,res) => {
+const getAllUsers = async (req,res) => {
     const users = await db.User.findAll();
 
     res.send(users);
 };
 
 const getUsersById = async (req,res) => {
-    const userId = req.params.id;
+    const userId = Number(req.params.id);
     const targetUser = await db.users.findOne({ where: {id: userId }});
 
+    res.send(targetUser)
 };
 const createNewUsers = (req,res) => {
 
