e0fc782c79d6631c45dbca83acc71e26b6e5bd11
uronohan <uronohan@gmail.com>
2020-05-27 15:16:15 +0700

    changes in controller

--- controllers/user.js
+++ controllers/user.js
@@ -7,18 +7,24 @@ const getAllUsers = async (req,res) => {
 };
 
 const getUsersById = async (req,res) => {
-    const userId = Number(req.params.id);
+    const user_id = Number(req.params.id);
     const targetUser = await db.users.findOne({ where: {id: user_id }});
 
     res.send(targetUser);
 };
-const createNewUsers = (req,res) => {
+const createNewUsers = async (req,res) => {
     const firstName = req.body.firstName;
     const lastName = req.body.lastname;
-    const email = req.body.email;
-    const password = Number(req.body.password);
-    
+    const userEmail = req.body.email;
+    const userPassword = Number(req.body.password);
 
+    const newUser = await db.User.create({
+        firstname: firstName,
+        lastname: lastName,
+        email: userEmail,
+        password: userPassword,
+    })
+    res.status(201).send(newUser);
 };
 const editUsersById = (req,res) => {
 
