9d91bb1b21df800fa429a3bb5a1aa15e10f7edb7
uronohan <uronohan@gmail.com>
2020-06-01 21:05:25 +0700

    changes in Post-controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,1 +1,6 @@
 const db = require('../models');
+
+const createPost = async (req,res) => {};
+const getAllPost = async (req,res) => {};
+const editPost = async (req,res) => {};
+const deletePost = async (req,res) => {};
