83997c452f58320a80079ffde3a0b56c5a2cf4af
uronohan <uronohan@gmail.com>
2020-06-02 21:23:26 +0700

    minor changes in Post-Controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,22 @@
 const db = require('../models');
 
-const createPost = async (req,res) => {};
-const getAllPost = async (req,res) => {};
+const createPost = async (req,res) => {
+    const { photo, text, timestamp} = req.body;
+
+    const post = await db.Post.create({
+        photo,
+        text,
+        timestamp,
+        user_id: req.user.id,
+    });
+
+    res.status(201).send(post);
+};
+const getAllPost = async (req,res) => {
+    const allPost = await db.post.findALL({ where: { user_id: req.user.id } });
+
+    res.send(allPost)
+};
 const editPost = async (req,res) => {};
 const deletePost = async (req,res) => {};
 
