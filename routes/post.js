cd6036d89557733826eef795379533931340ecf0
uronohan <uronohan@gmail.com>
2020-06-04 22:05:36 +0700

    minor changes in routes

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const { createPost, getAllPost, editPost, deletePost } = require('../controllers/post');
+const passport = require('passport');
 
+const auth = passport.authenticate('jwt', { session: false });
 
-
+router.post('/', auth, createPost)
+router.get('/', auth, getAllPost)
+router.put('/', auth, editPost)
+router.deletePost('/', auth, deletePost)
 
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/user')
+const { getAllUsers,getUsersById,createNewUsers,editUsersById,deleteUsersById } = require('../controllers/user');
+const passport = require('passport');
+
+const auth = passport.authenticate('jwt', { session: false });
 
 router.get('/', userController.getAllUsers);
 router.get('/', userController.getUsersById);
