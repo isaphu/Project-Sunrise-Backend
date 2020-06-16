a405ff6f6f663a4a3cd051f2f5efa62af9d1d8b8
uronohan <uronohan@gmail.com>
2020-06-16 11:11:02 +0700

    update on 4f

--- controllers/4S_search.js
+++ controllers/4S_search.js
@@ -3,8 +3,12 @@
 // URL =
 
 
-
-
-const getVenue = async (req,res) => {
-    const Venue = await
+const getVenue = async (req, res) => {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+    const venues = jsonResponse.response.groups[0].items.map(item => item.venue);
+      console.log(venues);
+      return venues;
+    } 
 }
